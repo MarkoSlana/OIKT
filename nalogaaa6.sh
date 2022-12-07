6cb8e3418282bd4f508f391fd9892b43670f9d81
MarkoSlana <120046721+MarkoSlana@users.noreply.github.com>
2022-12-07 21:17:18 +0100

    Add files via upload

---
+++ nalogaaa6.sh
@@ -0,0 +1,27 @@
+mkdir Documents Desktop Downloads Pictures Videos 
+for i in {1..5}
+do
+mkdir folder$i
+done
+
+for i in 1 2 3 4 5
+do
+useradd -m --groups sudo user$i
+done
+
+
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo dpgk --configure -a
+
+sudo apt-get install ufw
+sudo apt-get install git
+sudo apt-get install nginx -y
+sudo apt-get install net-tools
+
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
