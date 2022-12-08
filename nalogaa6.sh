c1b8c4f86932d9a931ae4e076a4b1f57c7db7665
MarkoSlana <120046721+MarkoSlana@users.noreply.github.com>
2022-12-08 19:25:11 +0100

    Update nalogaa6.sh

--- nalogaa6.sh
+++ nalogaa6.sh
@@ -32,5 +32,4 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt install docker -y
-sudo apt install docker.io -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
